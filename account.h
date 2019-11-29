14f9e031593ce13d056cff8058e54304a077739f
ALi-Natiq <46653906+ALi-Natiq@users.noreply.github.com>
2019-11-30 00:41:10 +0100

    Update account.h

--- account.h
+++ account.h
@@ -5,23 +5,35 @@ using namespace std;
 
 class account
 {
-
+private:
+    string name;
+    int deposit;
 public:
     int acno;
-    char name[30];
-    char type[20];
-    int deposit;
+    string type;
+    inline string getName(){
+          return name;
+       }
+    inline int getDeposit() const {
+          return deposit;
+       }
 
-    void create_account();	//function to get data from user
+    void setName (string name) {
+         this->name = name;
+      }
 
-    
+    void setDeposit(int deposit) {
+          this->deposit = deposit;
+       }
+
+    void create_account();
+
+    void show_account() const	;//function to show data on screen
+
+
+
 account();
     
 };
-
-
-
-
-
 
 #endif // BANK_ACCOUNT_H
