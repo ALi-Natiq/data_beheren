a1036b17de1ea5dc81dc9977ad2054bd26bb725f
ALi-Natiq <46653906+ALi-Natiq@users.noreply.github.com>
2019-11-30 00:40:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,8 +6,19 @@ using namespace std;
 
 int main()
 {
-   void account();
+       account obj;
+       obj.setName("Ali");
+       obj.setDeposit(100);
+       cout<<obj.getName()<<endl;
+       cout<<obj.getDeposit()<<endl;
+
+
+       obj.create_account();
+       //cout<<obj.create_account()<<endl;
+       obj.show_account();
+
 
 
     return 0;
 }
+
